ef67b5343ec6a1cf89356830acdeb1dfb9d79ae2
Mohammed Sulthan <sulthan@tericsoft.com>
2025-03-23 12:54:26 +0530

    chore: minor change

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,7 +3,6 @@ import '/constants/app_colors.dart';
 import '/constants/text_styles.dart';
 import '/views/famous_cities_weather.dart';
 import '/views/gradient_container.dart';
-import '/widgets/round_text_field.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -13,26 +12,7 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  late final TextEditingController _searchController;
-  bool _isSearchButtonAnimated = false;
   bool _isCardZoomed = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _searchController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _searchController.dispose();
-  }
-
-  void _onLocationIconClicked() {
-    // Navigate to home screen when location icon is clicked
-    Navigator.pushNamed(context, '/home');
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,40 +36,6 @@ class _SearchScreenState extends State<SearchScreen> {
         ),
         SizedBox(height: 40),
 
-        // Pick location row with animation
-        Row(
-          children: [
-            // Choose city text field with animated position
-            Expanded(
-              child: AnimatedPositioned(
-                duration: Duration(milliseconds: 500),
-                left: _isSearchButtonAnimated ? 0 : -300, // Start off-screen and animate in
-                child: RoundTextField(),
-              ),
-            ),
-            SizedBox(width: 15),
-
-            GestureDetector(
-              onTap: _onLocationIconClicked,
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                height: 55,
-                width: 55,
-                decoration: BoxDecoration(
-                  color: AppColors.accentBlue,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Icon(
-                  Icons.location_on_outlined,
-                  color: AppColors.grey,
-                ),
-              ),
-            ),
-          ],
-        ),
-
-        SizedBox(height: 30),
-
         // Famous cities weather cards with zoom animation
         GestureDetector(
           onTap: () {
@@ -103,9 +49,8 @@ class _SearchScreenState extends State<SearchScreen> {
             child: FamousCitiesWeather(),
           ),
         ),
-            SizedBox(height: 12), // Adds bottom margin
+        SizedBox(height: 12), // Adds bottom margin
       ],
     );
   }
 }
-
